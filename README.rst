680af347a6644ee3ee5c459ad0c2deee2e120639
Guillermo O. Freschi <tordek@tordek.com.ar>
2010-04-16 19:47:59 -0300

    Initial commit
    Signed-off-by: Guillermo O. Freschi <tordek@tordek.com.ar>

---
+++ README.rst
@@ -0,0 +1,55 @@
+Embiggen
+========
+
+**Embiggen lets you write HTML code faster.**
+
+You can write HTML in a CSS-like syntax, and have Embiggen handle the
+expansion to full HTML code. It is meant to help you write long HTML blocks
+in your text editor by letting you type fewer characters than needed.
+
+Embiggen is written in Python, and requires Python 2.5 or newer (2.5 is
+preinstalled in Mac OS X Leopard).
+
+Usage and installation
+----------------------
+You may download the latest version of **Embiggen** from Github.
+
+Afterwards, just call ``embiggen.py`` from you command line and start typing.
+
+Credits
+-------
+
+Embiggen is written by Guillermo Freschi and is released under the 2-clause
+BSD license. For more information, see the ``COPYING`` file.
+
+This project is inspired by `Vadim Makeev`_'s `Zen Coding`_ and
+`Rico Sta. Cruz`_'s `Sparkup`_.
+
+.. _`Vadim Makeev`: http://pepelsbey.net
+.. _`Zen Coding`: http://code.google.com/p/zen-coding/
+.. _`Rico Sta. Cruz`: http://ricostacruz.com
+.. _`Sparkup`: http://github.com/rstacruz/sparkup
+
+Examples
+--------
+
+``div`` expands to::
+
+    <div></div>
+
+``div#header`` expands to::
+
+    <div id="header"></div>
+
+``div.align-left#header`` expands to::
+
+    <div id="header" class="align-left"></div>
+
+``div#header + div#footer`` expands to::
+
+    <div id="header"></div>
+    <div id="footer"></div>
+
+``a[href=index.html]{Home}`` expands to::
+
+    <a href="index.html">Home</a>
---
+++ src/embiggen.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+"""Embiggen --- Simpler HTML.
+
+Embiggen is a simple filter program that takes a CSS-like syntax, and
+'embiggens' it, generating the corresponding HTML code.
+
+Embiggen is under the 2-clause BSD license. See the COPYING file for details.
+
+TODO: Multipliers.
+TODO: Placeholders.
+TODO: Command line options.
+TODO: Shortcuts.
+TODO: Default properties.
+TODO: Default block elements.
+"""
+
+import re
+import sys
+from xml.dom.minidom import Element, Text
+
+def build(element_unparsed):
+    """Builds an Element out of its string definition.
+
+    An element is composed of:
+
+    - A Name,
+    - Optionally, a class, preceded by #,
+    - Zero or more Classes, preceded and separated by `.`,
+    - Zero or more Properties, wrapped in `[]`, and optionally separated
+      by commas, and
+    - Optionally, content, wrapped in {}.
+
+    Some elements have inherent Properties (like `src` and `alt` for `img`).
+    Others are inherently block elements (like `div`).
+    """
+
+    result = re.match(r'(\w+)'                        # Name
+                      r'\s*(#\w+)?'                      # ID
+                      r'((?:\.\w+)*)'                 # Classes
+                      r'((?:\s*\[[^\]]+\])*)'         # Properties
+                      r'(?:\s*\{(\s*[^\}]+)\s*\})?',  # Content
+                      element_unparsed)
+
+    name, id_, classes, properties, content = result.group(1, 2, 3, 4, 5)
+
+    element = Element(name)
+
+    if id_ is not None:
+        element.setAttribute("id", id_)
+
+    if classes is not None and classes != '':
+        element.setAttribute("class", classes.replace('.', ' ').strip())
+
+    if properties is not None:
+        props = re.split(r'[\[\],]\s*', properties)
+        for prop in props:
+            if prop.strip() == '':
+                continue
+
+            if '=' in prop:
+                prop_name, prop_value = prop.split('=')
+            else:
+                prop_name, prop_value = prop, ''
+
+            element.setAttribute(prop_name.strip(), prop_value.strip())
+
+    if content is not None:
+        text = Text()
+        text.replaceWholeText(content)
+        element.appendChild(text)
+
+    return element
+
+def parse_element(line):
+    """Parses a single element out of a line.
+
+    Returns a triple of (element, separator, rest).
+    """
+
+    # This could be simplified. We only need to check that the separator isn't
+    # inside `[]` or `{}`.
+    result = re.search(r'(\w+(?:#\w+)?(?:\.\w+)*(?:\s*\[[^\]]+\])*(?:\s*\{[^\}]+\})?)'
+                        '(?:\s*(<|\+|>)(.*))?', line)
+    if result is not None:
+        element_unparsed, separator, rest = result.group(1, 2, 3)
+
+        return build(element_unparsed), separator, rest
+
+def parse(line):
+    """Parses a line, returning a tree representation of the result.
+
+    The root node is a dummy anonymous element.
+    """
+
+    root = Element('')
+    current_element = root
+    rest = line
+
+    while True:
+        element, separator, rest = parse_element(rest)
+
+        current_element.appendChild(element)
+
+        if rest is None:
+            break
+
+        if separator == '<':
+            current_element = current_element.parentNode
+        elif separator == '+':
+            current_element = current_element
+        elif separator == '>':
+            current_element = element
+
+    return root
+
+def decode(tree):
+    """Takes a parsed tree and generates the HTML.
+    """
+
+    result = ''
+
+    for subtree in tree.childNodes:
+        result += subtree.toxml()
+
+    return result
+
+def embiggen(line):
+    """Takes a line, parses it, and returns the generated HTML as a string.
+    """
+
+    return decode(parse(line))
+
+def main():
+    """Reads from `stdin` and generates the embiggened HTML for each line.
+    """
+    for line in sys.stdin:
+        print embiggen(line)
+
+if __name__ == "__main__":
+    main()
